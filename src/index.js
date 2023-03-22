9b155e86e058118d4d4b6f5baafd71d6a37e668e
Guzters <GaJR.B@outlook.com>
2023-03-21 20:04:53 -0500

    Added Express.js and a little message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello!')
+import express  from 'express'
+
+const app = express()
+
+const port = 3000
+
+app.get('/',(req,res)=> {
+    res.send({ msg: 'This is Gustavo!' })
+})
+
+app.listen(port)
