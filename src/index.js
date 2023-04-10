e9be223acadc942e14fc19a9bf6463b56f9561f7
Guzters <GaJR.B@outlook.com>
2023-04-09 22:35:13 -0500

    added basic model

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 import './utils/dotenv'
 
-import express  from 'express'
+import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
+import bodyParser from 'body-parser'
 
 import logger from './utils/logger'
 import router from './routes'
 import { notFound, errorHandler } from './utils/errors'
 
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = Number(process.env.port)
 
 const app = express()
 
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(cors({ origin: process.env.ORIGIN }))
 app.use(helmet())
+app.use(bodyParser.json())
 
-app.use(router)
+app.use('/', router)
 
 app.use(notFound)
 app.use(errorHandler)
 
 app.listen(port, () =>
-    logger.info(`Application started at http://localhost:${process.env.PORT}`)
+    logger.info(`Server running on port ${port}`)
     )
